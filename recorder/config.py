18763c4a9eb84e93c42253f93d2f2bc1a990f893
Sergio Pires <sergiodanpires@gmail.com>
2024-02-07 01:02:13 -0300

    Deletes python recorder

--- recorder/config.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("config.json") as config_file:
-    config = json.load(config_file)
-
-TOGGLE_RECORD = config["toggle_record"].lower()
-RECORD_MOUSE = config["record_mouse"].lower()
-RECORD_KEYBOARD = config["record_keyboard"].lower()
-SECONDS_RECORD = config["gif_record_seconds"]
